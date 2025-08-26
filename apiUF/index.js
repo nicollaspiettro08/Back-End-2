0b33c6877e8669e4dbc154f5638e546273616e5d
nicollaspiettro08 <damattanicollaspiettro@gmail.com>
2025-08-26 07:38:20 -0400

    d

---
+++ apiUF/index.js
@@ -0,0 +1,35 @@
+import colecaoUF from "./dados/dados.js";
+import express from 'express';
+
+const app = express();
+
+app.get('/ufs',(req,res) => {
+    res.json(colecaoUF)
+});
+
+app.get('/ufs/idUF',(req,res) => {
+    const idUF = parseInt(req.params.idUF);
+    let mensagemErro = '';
+    let uf;
+
+    if(!(isNaN(idUF))){
+        uf = colecaoUF.find(u => u.id == idUF);
+        if (!uf){
+            mensagemErro = 'UF não encontrada';
+        }
+        else{
+            mensagemErro = 'Requisição invalida';
+        }
+    }
+
+    if(uf){
+        res.json(uf);
+    }else{
+        res.status(404).json({"erro": mensagemErro});
+    }
+});
+
+app.listen(8080,() =>{
+    let data = new Date();
+    console.log('Servidor iniciado na porta 8080 em:' +data)
+});  
