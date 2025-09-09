544658bf173b99c6ec3fb6d067ba4795f7f121c9
nicollaspiettro08 <damattanicollaspiettro@gmail.com>
2025-09-09 09:07:44 -0400

    09/09/25

--- apiUF/index.js
+++ apiUF/index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { buscarUfs,buscarUfsPorNome,buscarUfsPorId } from "./servicos/servico.js";
+import { buscarUfs, buscarUfsPorNome, buscarUfsPorId, buscarUfPorSigla, buscarUfsPorInicial } from "./servicos/servico.js";
 
 const app = express();
 
@@ -10,23 +10,49 @@ app.get('/ufs', (req, res) => {
     if (resultado.length > 0) {
         res.json(resultado);
     } else {
-        res.status(404).json({ "erro": "Nenhuma UF encontrada" });
+        res.status(404).json({ erro: "Nenhuma UF encontrada" });
     }
 });
 
 app.get('/ufs/:idUF', (req, res) => {
-    const uf = buscarUfsPorId(req.params.idUF);
+    const idUF = req.params.idUF;
+    const uf = buscarUfsPorId(idUF);
+
+    if (uf) {
+        res.json(uf);
+    } else if (isNaN(parseInt(idUF))) {
+        res.status(400).send({ erro: "Requisição inválida" });
+    } else {
+        res.status(404).send({ erro: "UF não encontrada" });
+    }
+});
+
+app.get('/ufs/sigla/:sigla', (req, res) => {
+    const sigla = req.params.sigla;
+    const uf = buscarUfPorSigla(sigla);
     
     if (uf) {
         res.json(uf);
-    } else if(isNaN(parseInt9req.params.idUF)){
-        res.status(404).send({ "erro": "Requisição invalida" });
-    }else {
-        res.status(404).send({ "erro": "Uf não encontrada" });
+    } else {
+        res.status(404).json({ erro: "UF não encontrada pela sigla" });
+    }
+});
+
+app.get('/ufs/:valor', (req, res) => {
+    const valor = req.params.valor;
+    let resultado = [];
+
+    if (valor.length === 1) {
+         resultado = buscarUfsPorInicial(valor);
+    }
+    if (resultado.length > 0) {
+        res.json(resultado);
+    } else {
+        res.status(404).json({ erro: "UF não encontrada pela inicial" });
     }
 });
 
 app.listen(8080, () => {
-    let data = new Date();
-    console.log('Servidor iniciado na porta 8080 em: ' + data);
-});  
+    const data = new Date().toLocaleString("pt-BR");
+    console.log(` Servidor iniciado na porta 8080 em: ${data}`);
+});
--- apiUF/servicos/servico.js
+++ apiUF/servicos/servico.js
@@ -1,14 +1,26 @@
-import colecaoUF from "../dados/dados";
+import colecaoUF from "../dados/dados.js"; 
 
-export const buscarUfs = ()=>{
+export const buscarUfs = () => {
     return colecaoUF;
-}
-
-export const buscarUfsPorNome = (nomeUF) => {
-    return colecaoUF.filter(uf => uf.nome.toLowerCase().includes(nomeUF.toLowerCase()));
 };
 
-export const buscarUfsPorId = (id) =>{
+export const buscarUfsPorNome = (nomeUF) => {
+    return colecaoUF.filter(uf => 
+        uf.nome.toLowerCase().includes(nomeUF.toLowerCase())
+    );
+};
+
+export const buscarUfsPorId = (id) => {
     const idUF = parseInt(id);
     return colecaoUF.find(uf => uf.id === idUF);
-}
+};
+
+
+export const buscarUfPorSigla = (sigla) => {
+    return colecaoUf.find(uf => uf.uf.toLowerCase() === sigla.toLowerCase());
+};
+
+
+export const buscarUfsPorInicial = (letra)=> {
+    return colecaoUF.find(uf => uf.uf.toLowerCase().startsWith(letra.toLowerCase()));
+};
